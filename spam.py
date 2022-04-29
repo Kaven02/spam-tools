32d53826b7f248fce9abb654bcfe4e6ba9f5232a
Kaven <92474373+KavenKV@users.noreply.github.com>
2022-04-29 08:35:09 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,1 +1,14 @@
-
+import pyautogui, pyperclip, random, time
+messenger_ = input("Nội dung: ").split(" || ")
+times_ = int(input("Số lần=>>"))
+delay_ = float(input("Delay=>>"))
+print("Ready!")
+for i in range(3,0,-1):
+	print(i,flush='True')
+	time.sleep(1)
+print("Spam!")
+for i in range(times_):
+	pyperclip.copy(random.choice(messenger_))
+	pyautogui.hotkey("ctrl","v")
+	pyautogui.press("enter")
+	time.sleep(delay_)
